4cc05dc50fcba9c2741dd5e0d9b1f036e8a079cc
SundaePorkCutlet <wns301@naver.com>
2024-06-12 00:29:19 +0900

    error fix

--- main.tf
+++ main.tf
@@ -77,13 +77,6 @@ module "elb_http" {
       lb_port           = 80
       lb_protocol       = "HTTP"
     },
-    {
-      instance_port     = 8080
-      instance_protocol = "http"
-      lb_port           = 8080
-      lb_protocol       = "http"
-      ssl_certificate_id = "arn:aws:acm:eu-west-1:235367859451:certificate/6c270328-2cd5-4b2d-8dfd-ae8d0004ad31"
-    },
   ]
 
   health_check = {
