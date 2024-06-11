1f5e885c22edebe3f43383bc235336d86050a5bc
SundaePorkCutlet <wns301@naver.com>
2024-06-11 19:30:33 +0900

    add elb

--- main.tf
+++ main.tf
@@ -61,6 +61,52 @@ resource "aws_instance" "web" {
   }
 }
 
+module "elb_http" {
+  source  = "terraform-aws-modules/elb/aws"
+
+  name = "web-eld"
+
+  subnets         = module.default_vpc.public_subnets
+  security_groups = [module.default_vpc.default_security_group_id]
+  internal        = false
+
+  listener = [
+    {
+      instance_port     = 80
+      instance_protocol = "HTTP"
+      lb_port           = 80
+      lb_protocol       = "HTTP"
+    },
+    {
+      instance_port     = 8080
+      instance_protocol = "http"
+      lb_port           = 8080
+      lb_protocol       = "http"
+      ssl_certificate_id = "arn:aws:acm:eu-west-1:235367859451:certificate/6c270328-2cd5-4b2d-8dfd-ae8d0004ad31"
+    },
+  ]
+
+  health_check = {
+    target              = "HTTP:80/"
+    interval            = 30
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 5
+  }
+
+  access_logs = {
+    bucket = "my-access-logs-bucket"
+  }
+
+  // ELB attachments
+  number_of_instances = 2
+  instances           = aws_instance.web.*.id
+  tags = {
+    Owner       = "user"
+    Environment = "dev"
+  }
+}
+
 # module "custom_vpc" {
 #   source = "./custom_vpc"
 #   env = "dev"
